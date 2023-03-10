1f8be321eeb39ea2e35ba04ce3d9c7d017237466
Alisia Rush <alisiarush@gmail.com>
2023-03-10 15:46:11 -0500

    Added list elements using props

--- src/components/products.js
+++ src/components/products.js
@@ -1,33 +1,30 @@
 import "./products.css";
 
 let productCount = 0;
-let freshMilkImage = require("../images/freshMilk.png");
-let isAvailable = "Available";
-
-let badgeClass = "bage-margin-left-240 badge";
-badgeClass += isAvailable === "Available" ? " bg-success " : " bg-danger ";
 
 function displayFormattedProductCount() {
   return productCount > 0 ? productCount : "Zero";
 }
 
-function Products() {
+function Products(props) {
+  let badgeClass = "bage-margin-left-240 badge";
+  badgeClass += props.isAvailable ? " bg-success" : " bg-danger ";
+
   return (
     <ul className="list-group shadow">
       <li className="list-group-item">
         <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
           <div className="media-body order-2 order-lg-1">
-            <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
+            <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
             <p className="font-italic text-muted mb-0 small">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Suscripit fuga autem maiores
+              {props.description}
             </p>
             <div className="d-flex align-items-center justify-content-start mt-1">
               <h6
                 className="font-weight-bold my-2"
                 style={{ marginRight: "30px" }}
               >
-                $120.00
+                {props.price}
               </h6>
               <button className="btn btn-primary">-</button>
               <span style={{ padding: "0 20px", fontSize: 13 }}>
@@ -35,11 +32,13 @@ function Products() {
               </span>
               <button className="btn btn-primary">+</button>
 
-              <span className={badgeClass}>{isAvailable}</span>
+              <span className={badgeClass}>
+                {props.isAvailable ? "Available" : "Unavailable"}
+              </span>
             </div>
           </div>
           <img
-            src={freshMilkImage}
+            src={props.imageUrl}
             className="ml-lg-5 order-1 order-lg-2"
             alt=""
           />
