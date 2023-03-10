93062e0a4471bfab0012bcb1a2c7594eb75d1982
Alisia Rush <alisiarush@gmail.com>
2023-03-10 17:25:04 -0500

    name change on files product.js to Product.js and products.css to Products.css

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,15 @@
 import React from "react"; // manditory in older React Apps only. Good practice to include anyway
 
+function btnClickEventHandler() {
+  console.log("button clicked");
+}
+
 function Button(props) {
-  return <button className="btn btn-primary">{props.children}</button>;
+  return (
+    <button onClick={btnClickEventHandler} className="btn btn-primary">
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
