0e046ddd4db6bf5592af9376ae285e8e9a8cb252
Alisia Rush <alisiarush@gmail.com>
2023-03-10 11:36:26 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const header = <h1>This is a basic React App</h1>;
+
+ReactDOM.createRoot(document.getElementById("root")).render(header);
