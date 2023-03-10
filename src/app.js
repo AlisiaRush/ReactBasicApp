2d02778cdceb9fe75285bcdefd0158f81576c85c
Alisia Rush <alisiarush@gmail.com>
2023-03-10 11:55:17 -0500

    Added jsx to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 // create using javaScript function
 function App() {
-  return <p>This is the first App Component</p>;
+  let message = "This is a new message";
+  return (
+    <div>
+      <h1>This is an h1 element</h1>
+      <p>This is the first App Component. {message}</p>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app.js";
 
-const header = <h1>This is a basic React App</h1>;
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
