87711153dd2deee345d80d21beda3c74135f5b6b
Alisia Rush <alisiarush@gmail.com>
2023-03-10 12:09:05 -0500

    Created a child component called Products and imported it into the main app.js component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
 // create using javaScript function
 import "./app.css";
+import Products from "./components/products";
 
 function App() {
-  let message = "This is a new message";
-  return (
-    <div>
-      <h1 className="myHeader">This is an h1 element</h1>
-      <p className="myParagraph">This is the first App Component. {message}</p>
-    </div>
-  );
+  return <Products />;
 }
 
 export default App;
