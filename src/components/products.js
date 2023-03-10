778c4bc4faf427373eddb1fff4345c6c802f3dec
Alisia Rush <alisiarush@gmail.com>
2023-03-10 12:43:36 -0500

    Added jsx layout for DOM

--- src/components/products.js
+++ src/components/products.js
@@ -1,5 +1,25 @@
 function Products() {
-  return <h1>All Products</h1>;
+  return (
+    <ul>
+      <li>
+        <div>
+          <div>
+            <h5>Fresh Milk</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Suscripit fuga autem maiores
+            </p>
+            <div>
+              <h6>$120.00</h6>
+            </div>
+          </div>
+          <img src="" width="200" />
+        </div>
+      </li>
+    </ul>
+  );
 }
+
+// React.createElement();
 
 export default Products;
