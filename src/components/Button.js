6982088226e603ba4dd42c402a5df5948cf2f865
Alisia Rush <alisiarush@gmail.com>
2023-03-13 09:41:59 -0400

    Added useState Hook to increment products and call function each time user clicks buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,8 @@
 import React from "react"; // manditory in older React Apps only. Good practice to include anyway
 
-function btnClickEventHandler() {
-  console.log("button clicked");
-}
-
 function Button(props) {
   return (
-    <button onClick={btnClickEventHandler} className="btn btn-primary">
+    <button onClick={props.eventHandler} className="btn btn-primary">
       {props.children}
     </button>
   );
--- src/components/productDetails.js
+++ src/components/productDetails.js
@@ -1,26 +1,42 @@
-import React from "react"; // manditory in older React Apps only. Good practice to include anyway
+import React, { useState } from "react"; // manditory in older React Apps only. Good practice to include anyway
 import Button from "./Button";
 
-let productCount = 0;
-
-function displayFormattedProductCount() {
-  return productCount > 0 ? productCount : "Zero";
-}
-
 function ProductDetails(props) {
+  //   let productCount = 0;
+
+  let [productCount, updateCount] = useState(0);
+
+  function displayFormattedProductCount() {
+    return productCount > 0 ? productCount : "Zero";
+  }
+
   let badgeClass = "bage-margin-left-240 badge";
   badgeClass += props.isAvailable ? " bg-success" : " bg-danger ";
+
+  let incrementProductCount = function () {
+    // updateCount(productCount++);
+    updateCount(++productCount);
+    // productCount++;
+    // console.log(productCount);
+  };
+
+  let decrementProductCount = function () {
+    // updateCount(productCount--);
+    updateCount(--productCount);
+    // productCount--;
+    // console.log(productCount);
+  };
 
   return (
     <div className="d-flex align-items-center justify-content-start mt-1">
       <h6 className="font-weight-bold my-2" style={{ marginRight: "30px" }}>
         {props.price}
       </h6>
-      <Button>-</Button>
+      <Button eventHandler={decrementProductCount}>-</Button>
       <span style={{ padding: "0 20px", fontSize: 13 }}>
         {displayFormattedProductCount()}
       </span>
-      <Button>+</Button>
+      <Button eventHandler={incrementProductCount}>+</Button>
 
       <span className={badgeClass}>
         {props.isAvailable ? "Available" : "Unavailable"}
